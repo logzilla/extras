4c3e46e99a2220072b103ee2dafb8db30419661b
Clay <support@logzilla.net>
2022-05-05 20:02:56 +0000

    mkhosts update

--- contrib/mkhosts.sh
+++ contrib/mkhosts.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# This script can be used to easily create
+# a hosts file for environments that do
+# not have reverse DNS available
+# but still want hostnames instead of IP's
+# to show up in the UI
+
+# Note: Requires jq (apt install -y jq)
+
+# Obtained from 'logzilla authtoken create':
 token="ac3e5526f03b77f3f0f4d316904495ce579cb51d2e53a508"
+apiURL="http://192.168.10.135/api"
 hostsFile="/etc/logzilla/hosts.in"
 
 declare -A entries
-ips=($(curl -sH "Content-Type: application/json; charset=utf-8" -H "Authorization: token $token" "http://192.168.10.135/api/dictionaries/host?limit=1000" | jq -r '.list[].name' | grep -P '^\d{1,3}\.'))
+ips=($(curl -sH "Content-Type: application/json; charset=utf-8" -H "Authorization: token $token" "$apiURL/dictionaries/host?limit=1000" | jq -r '.list[].name' | grep -P '^\d{1,3}\.'))
 
 echo
 for ip in "${ips[@]}"; do
