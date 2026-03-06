ee6016de746253ca8c219386e37cef3f824752b9
Clayton Dukes <cdukes@logzilla.net>
2026-03-05 23:18:26 -0500

    Remove winagent_source/ - moved to github.com/logzilla/winagent

--- winagent_source/source/AgentLib-Test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-// Basic test to verify the test framework
-TEST(SanityCheck, BasicAssertion) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
-
-// Main function provided by Google Test
--- winagent_source/source/AgentLib/INetworkClient.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-// #include "stdafx.h" // Removed, pch.h is used
-#include "INetworkClient.h"
-
-namespace Syslog_agent {
-
-	const INetworkClient::RESULT_TYPE INetworkClient::RESULT_SUCCESS(NetworkErrorCode::Success, "Success");
-
-} // namespace Syslog_agent
